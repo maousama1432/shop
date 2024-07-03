1a0a9da379e27a5e33c0e563f1336a67a5e90095
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2024-07-03 09:55:37 -0400

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,43 @@
-// src/components/Main.js
+"use client";
 
 import React, { useEffect, useState } from 'react';
 import './styles/Main.css';
 
-const Main = () => {
+const Main = ({ cart, setCart }) => {
   const [products, setProducts] = useState([]);
-
+  
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then(response => response.json())
       .then(data => setProducts(data));
   }, []);
 
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (index) => {
+    const newCart = [...cart];
+    newCart.splice(index, 1);
+    setCart(newCart);
+  };
+
   return (
     <main className="main">
-      {products.map(product => (
-        <div key={product.id} className="product">
-          <h2>{product.title}</h2>
-          <p>Preço: ${product.price}</p>
-          <p>{product.description}</p>
-          <p>Categoria: {product.category}</p>
-          <img src={product.image} alt={product.title} />
-          <p>Avaliações: {product.rating.count}</p>
-        </div>
-      ))}
+      <div className="products">
+        {products.map(product => (
+          <div key={product.id} className="product">
+            <h2>{product.title}</h2>
+            <p>Preço: ${product.price}</p>
+            <p>{product.description}</p>
+            <p>Categoria: {product.category}</p>
+            <img src={product.image} alt={product.title} />
+            <p>Avaliações: {product.rating.count}</p>
+            <button onClick={() => addToCart(product)}>Adicionar ao Carrinho</button>
+          </div>
+        ))}
+      </div>
+
     </main>
   );
 };
